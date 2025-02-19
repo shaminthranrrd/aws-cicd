cab0d0f322c0779c976cf1a1a973782445532941
shaminthranrrd <shaminthranr@gmail.com>
2025-02-19 13:10:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.send("Hello, AWS Elastic Beanstalk!");
+    res.send("Hello, AWS Elastic Beanstalk-CICDWELCOM!");
 });
 
 app.listen(PORT, () => {
