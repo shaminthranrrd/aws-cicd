5e67133876be5e9313a079de1ccc6aec16ca7aa6
shaminthranrrd <shaminthranr@gmail.com>
2025-02-21 10:45:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const app = express();
 
+// Redirect HTTP to HTTPS
+app.use((req, res, next) => {
+    if (process.env.FORCE_HTTPS && req.headers["x-forwarded-proto"] !== "https") {
+        return res.redirect("https://" + req.headers.host + req.url);
+    }
+    next();
+});
+
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.send("Hello, AWS Elastic Beanstalk-CICDWELCOM!");
+    res.send("Hello, AWS Elastic Beanstalk with HTTPS!");
 });
 
 app.listen(PORT, () => {
